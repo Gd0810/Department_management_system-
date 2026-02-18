c9fa9684884f01b0c0ef2f33bf55f27d36be859d
gd0810 <zeefer2000@gmail.com>
2026-02-18 16:59:38 +0530

    add:departmetn_admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Department, Worker, Project, ProjectMember, distribute_project_payment
+from django.contrib.auth.hashers import make_password
 
-# Register your models here.
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
+
+    def save_model(self, request, obj, form, change):
+        if not change or "password" in form.changed_data:
+            obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)
