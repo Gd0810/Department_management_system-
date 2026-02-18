75cec67ed31d0973821182d35ba04e88ca7e2fb8
gd0810 <zeefer2000@gmail.com>
2026-02-18 16:44:35 +0530

    update:crating_worker_model

--- dashboard/models.py
+++ dashboard/models.py
@@ -16,3 +16,21 @@ class Department(models.Model):
         return self.name
 
 
+class Worker(models.Model):
+
+    WORKER_TYPE = (
+        ('staff', 'Staff'),
+        ('intern', 'Intern'),
+    )
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="workers")
+
+    worker_type = models.CharField(max_length=10, choices=WORKER_TYPE)
+    name = models.CharField(max_length=150)
+    date_of_join = models.DateField()
+    image = models.ImageField(upload_to='workers/', blank=True, null=True)
+    posting = models.CharField(max_length=150)
+    department_role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.worker_type})"
