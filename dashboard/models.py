630d284680ccfd60fbb38606c983f222bf848361
gd0810 <zeefer2000@gmail.com>
2026-02-18 16:43:17 +0530

    update:crating_department_model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from decimal import Decimal
 
-# Create your models here.
+
+
+class Department(models.Model):
+
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="departments/", blank=True, null=True)
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # will store hashed password
+
+    def __str__(self):
+        return self.name
+
+
