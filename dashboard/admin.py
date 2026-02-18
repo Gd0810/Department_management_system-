262a43cde2b11cf550e27532e5f5d16d741f52f2
gd0810 <zeefer2000@gmail.com>
2026-02-18 17:02:28 +0530

    add:project_member_admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -22,3 +22,21 @@ class WorkerAdmin(admin.ModelAdmin):
 class ProjectMemberInline(admin.TabularInline):
     model = ProjectMember
     extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "category", "status", "work_type", "amount")
+    list_filter = ("category", "status", "work_type")
+
+    inlines = [ProjectMemberInline]
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        distribute_project_payment(form.instance)
+
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    list_display = ("project", "worker", "contribution", "payment")
+
+
