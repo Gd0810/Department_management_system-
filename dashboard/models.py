b3f82af8a8fbba8f3d8702755598848f93727131
gd0810 <zeefer2000@gmail.com>
2026-02-18 16:46:24 +0530

    update:crating_project_model

--- dashboard/models.py
+++ dashboard/models.py
@@ -34,3 +34,47 @@ class Worker(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.worker_type})"
+    
+
+class Project(models.Model):
+
+    PROJECT_CATEGORY = (
+        ('client', 'Client'),
+        ('company', 'Company'),
+        ('internship', 'Internship'),
+        ('academy', 'Academy'),
+    )
+
+    PROJECT_STATUS = (
+        ('started','Started'),
+        ('ongoing','Ongoing'),
+        ('on_hold','On Hold'),
+        ('canceled','Canceled'),
+        ('finished','Finished'),
+    )
+
+    WORK_TYPE = (
+        ('solo','Solo'),
+        ('group','Group'),
+    )
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="projects")
+
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=20, choices=PROJECT_CATEGORY)
+    work_type = models.CharField(max_length=10, choices=WORK_TYPE)
+    start_date = models.DateField()
+    status = models.CharField(max_length=20, choices=PROJECT_STATUS)
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def clean(self):
+        if self.category == 'company' and self.amount:
+            raise ValidationError("Company projects cannot have amount")
+
+        if self.category != 'company' and not self.amount:
+            raise ValidationError("This project type requires amount")
+
+    def __str__(self):
+        return f"{self.title} ({self.category})"
+    
