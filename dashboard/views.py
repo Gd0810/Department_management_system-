43f26b0f03973ff13bc80693e7b5c38d48070d2b
gd0810 <zeefer2000@gmail.com>
2026-02-19 10:16:40 +0530

    update:calculate_project_payments

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Department
 from django.contrib.auth.hashers import check_password
+from collections import defaultdict
+from decimal import Decimal
+
 
 
 # =========================
@@ -65,3 +68,58 @@ def dashboard(request):
 def logout_view(request):
     request.session.flush()
     return redirect("login")
+
+def calculate_project_payments(project):
+
+    if not project.amount:
+        return []
+
+    members = list(project.members.select_related("worker"))
+
+    gold = [m for m in members if m.contribution == "gold"]
+    silver = [m for m in members if m.contribution == "silver"]
+    copper = [m for m in members if m.contribution == "copper"]
+
+    total_amount = Decimal(project.amount)
+
+    payments = {}
+
+    # Only gold → equal
+    if gold and not silver and not copper:
+        share = total_amount / len(gold)
+        for m in gold:
+            payments[m.id] = share
+
+    # Gold + Silver
+    elif gold and silver and not copper:
+        gold_total = total_amount * Decimal("0.60")
+        silver_total = total_amount * Decimal("0.40")
+
+        for m in gold:
+            payments[m.id] = gold_total / len(gold)
+
+        for m in silver:
+            payments[m.id] = silver_total / len(silver)
+
+    # Gold + Copper
+    elif gold and copper and not silver:
+        gold_total = total_amount * Decimal("0.70")
+        copper_total = total_amount * Decimal("0.30")
+
+        for m in gold:
+            payments[m.id] = gold_total / len(gold)
+
+        for m in copper:
+            payments[m.id] = copper_total / len(copper)
+
+    # Fallback weight system
+    else:
+        weight_map = {"gold":3, "silver":2, "copper":1}
+        total_weight = sum(weight_map[m.contribution] for m in members)
+
+        for m in members:
+            share = (Decimal(weight_map[m.contribution]) / Decimal(total_weight)) * total_amount
+            payments[m.id] = share
+
+    return payments
+
