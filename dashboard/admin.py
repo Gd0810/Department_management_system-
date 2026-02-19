9fb09289cfab9d8b95966ededa28ca82b3dab194
gd0810 <zeefer2000@gmail.com>
2026-02-19 10:22:57 +0530

    update:admin_logices

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Department, Worker, Project, ProjectMember, distribute_project_payment
 from django.contrib.auth.hashers import make_password
+from .models import Department, Worker, Project, ProjectMember
 
 
 
@@ -9,9 +9,11 @@ class DepartmentAdmin(admin.ModelAdmin):
     list_display = ("name", "email")
 
     def save_model(self, request, obj, form, change):
+        # hash password only when changed or created
         if not change or "password" in form.changed_data:
             obj.password = make_password(obj.password)
         super().save_model(request, obj, form, change)
+
 
 @admin.register(Worker)
 class WorkerAdmin(admin.ModelAdmin):
@@ -19,24 +21,32 @@ class WorkerAdmin(admin.ModelAdmin):
     list_filter = ("worker_type", "department")
     search_fields = ("name",)
 
+
 class ProjectMemberInline(admin.TabularInline):
     model = ProjectMember
     extra = 1
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "worker":
+            if request.resolver_match.kwargs.get("object_id"):
+                project_id = request.resolver_match.kwargs.get("object_id")
+                project = Project.objects.filter(id=project_id).first()
+                if project:
+                    kwargs["queryset"] = Worker.objects.filter(department=project.department)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
 
-    list_display = ("title", "category", "status", "work_type", "amount")
-    list_filter = ("category", "status", "work_type")
+    list_display = ("title", "department", "category", "status", "work_type", "amount")
+    list_filter = ("category", "status", "work_type", "department")
+    search_fields = ("title",)
 
     inlines = [ProjectMemberInline]
 
-    def save_related(self, request, form, formsets, change):
-        super().save_related(request, form, formsets, change)
-        distribute_project_payment(form.instance)
 
 @admin.register(ProjectMember)
 class ProjectMemberAdmin(admin.ModelAdmin):
-    list_display = ("project", "worker", "contribution", "payment")
-
-
+    list_display = ("project", "worker", "contribution")
+    list_filter = ("contribution",)
