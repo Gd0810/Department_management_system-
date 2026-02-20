efc5b9d37efc4e09ef7a123a3f4e73012d188ef7
gd0810 <zeefer2000@gmail.com>
2026-02-20 13:13:25 +0530

    add:base_template_file

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
 from .import views 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.login_page, name="login"),
     path('login/', views.login_page, name="login"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('logout/', views.logout_view, name="logout"),
+    path('demo/',TemplateView.as_view(template_name="base.html"), name="demo"),
  ]
