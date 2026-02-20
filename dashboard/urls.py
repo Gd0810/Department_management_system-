8a86d04545fb244d06def6c88ee88e6dec765226
gd0810 <zeefer2000@gmail.com>
2026-02-20 15:36:28 +0530

    upate:the_navbar_for_htmx

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,15 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.login_page, name="login"),
     path('login/', views.login_page, name="login"),
-    path('dashboard/', views.dashboard, name="dashboard"),
-    path('logout/', views.logout_view, name="logout"),
-    path('demo/',TemplateView.as_view(template_name="base.html"), name="demo"),
+    
+    path('', views.base, name="base"),
+
+    path('index/', views.index, name="index"),
+    path('team/', views.team, name="team"),
+    path('client/', views.client, name="client"),
+    path('company/', views.company, name="company"),
+    path('academics/', views.academics, name="academics"),
+    path('internship/', views.internship, name="internship"),
+    path('add-team/', views.add_team, name="add_team"),
+    path('add-project/', views.add_project, name="add_project"),
  ]
