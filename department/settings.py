535639582a86957dfb8e9837c4dc065711f51d4d
gd0810 <zeefer2000@gmail.com>
2026-02-24 13:17:53 +0530

    update:database_switch_to_postgresesql

--- department/settings.py
+++ department/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'department.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'income_management',  
+        'USER': 'postgres',   
+        'PASSWORD': '1234', 
+        'HOST': 'localhost',           
+        'PORT': '',                     
     }
 }
+
 
 
 # Password validation
