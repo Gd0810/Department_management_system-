86e5cdd5630e24928e3e0a62fe85aa2096efef15
gd0810 <zeefer2000@gmail.com>
2026-04-03 16:12:39 +0530

    add:build_css_file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./dashboard/templates/**/*.html"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
